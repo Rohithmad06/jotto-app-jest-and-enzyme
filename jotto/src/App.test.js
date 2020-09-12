29408fb44af38049d540ff502957995d000f93e2
Rohithmad06 <39017970+Rohithmad06@users.noreply.github.com>
2020-09-12 21:25:43 +0530

    Initial commit

---
+++ jotto/src/App.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Enzyme, { shallow, ShallowWrapper } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
+
+test("renders learn react link", () => {});
